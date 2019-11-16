e3afedec8f92af3d5a0993ea78e20b965db48924
Yurie <cutecupcakeandcute@gmail.com>
2019-11-17 00:29:48 +0900

    export and import TaskList component into root

--- components/TaskList.vue
+++ components/TaskList.vue
@@ -1,6 +1,6 @@
 <template>
     <div>
-        <h3></h3>
+        <h3>Task List</h3>
         <p></p>
     </div>
 </template>
--- pages/index.vue
+++ pages/index.vue
@@ -1,13 +1,15 @@
 <template>
   <v-layout column justify-center align-center>
     <v-flex xs12 sm8 md6 />
+    <TaskList />
   </v-layout>
 </template>
 
 <script>
-
+import TaskList from '../components/TaskList'
 export default {
   components: {
+    TaskList
   }
 }
 </script>
