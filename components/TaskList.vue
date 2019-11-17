6a440a5f9cf81914ccb5924961ecbfc79f714949
Yurie <cutecupcakeandcute@gmail.com>
2019-11-18 01:24:43 +0900

    use checkbox component from Vuetify (why does it work without the code in <script>)

--- components/TaskList.vue
+++ components/TaskList.vue
@@ -2,6 +2,7 @@
     <div>
         <h3>Task List</h3>
         <div v-for="item in getData" :key="item.id">
+            <v-checkbox v-model="checkbox"></v-checkbox>
             <h4>{{item.name}}</h4>
             <p>{{item.description}}</p>
         </div>
