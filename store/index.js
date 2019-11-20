ff2dc5b3bd9056d0e85825f521dfd04f721f7bad
Yurie <cutecupcakeandcute@gmail.com>
2019-11-21 03:10:35 +0900

    add comments and methods to be used later. (mutation)

--- store/index.js
+++ store/index.js
@@ -1,14 +1,27 @@
 export const state = () => ({
+  // JSON Object
   tasks: [
     {
       'id': 0,
       'name': 'create repo',
-      'description': 'Make sure to install Vuex & Vuetify'
+      'description': 'Make sure to install Vuex & Vuetify',
+      'checked': false
     },
     {
       'id': 1,
       'name': 'Create Task list component',
-      'description': 'Allow user to check and un-check'
+      'description': 'Allow user to check and un-check',
+      'checked': false
     }
   ]
+
+  // getters: {
+  //   Tasks: (state) => {
+  //     return state.tasks
+  //   }
+  // }
+
+  // changing state should be done by commiting a mutation
+  // mutation: {
+  // }
 })
