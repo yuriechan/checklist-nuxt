b198377dd143923d93014149c7a870f11e6487c9
Yurie <cutecupcakeandcute@gmail.com>
2019-11-21 03:11:23 +0900

    move data from computed() to data() & add some comments/questions to ask later

--- components/TaskList.vue
+++ components/TaskList.vue
@@ -1,7 +1,7 @@
 <template>
     <div>
         <h3>Task List</h3>
-        <div v-for="item in getData" :key="item.id">
+        <div v-for="item in taskList" :key="item.id">
             <v-checkbox v-model="checkbox"></v-checkbox>
             <h4>{{item.name}}</h4>
             <p>{{item.description}}</p>
@@ -11,11 +11,24 @@
 
 <script>
 export default {
-    computed: {
-        getData () {
-            return this.$store.state.tasks
+    // Getting the state from store should be done through functions
+    // Methods inside the `computed` will be cached, while they will not inside `methods`
+
+    // data must be in function form, not object => why? 
+    // The following code would not work
+    // data: {
+    //     taskList () {
+    //         return this.$store.state.tasks
+    //     }
+    // }
+
+    // checkbox binding will be either false or true
+    data: function () {
+        return {
+            taskList : this.$store.state.tasks
         }
     }
+
 }
 </script>
 
