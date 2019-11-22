e871a350e6bf7c2c6882bc6d99796ae8e6ce8a80
Yurie <cutecupcakeandcute@gmail.com>
2019-11-22 19:59:51 +0900

    create setter to update state when user input happens (not completed yet)

--- components/TaskForm.vue
+++ components/TaskForm.vue
@@ -5,10 +5,10 @@
     <v-container>
         <v-row>
             <v-col cols="12">
-                <v-text-field label="Task name"></v-text-field>
+                <v-text-field label="Task name" v-model="taskName" :rules="setTaskName"></v-text-field>
             </v-col>
             <v-col cols="12">
-                <v-textarea label="Task description"></v-textarea>
+                <v-textarea label="Task description" v-model="taskDescription"></v-textarea>
             </v-col>
         </v-row>
     </v-container>
@@ -17,7 +17,19 @@
 </template>
 
 <script>
-
+export default {
+    data: () => {
+        return {
+            taskName: '',
+            taskDescription: ''
+        }
+    },
+    computed: {
+        setTaskName (value) {
+            this.taskName = value
+        } 
+    }
+}
 </script>
 
 <style scoped>
