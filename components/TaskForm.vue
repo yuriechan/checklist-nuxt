4f4592ab391c8c4c0ebcbe69bcf9bc19888093fc
Yurie <cutecupcakeandcute@gmail.com>
2019-11-23 16:05:59 +0900

    create setter and getter for computed properties when task description detects user input

--- components/TaskForm.vue
+++ components/TaskForm.vue
@@ -8,7 +8,7 @@
                 <v-text-field label="Task name" v-model="taskName"></v-text-field>
             </v-col>
             <v-col cols="12">
-                <v-textarea label="Task description" v-model="description"></v-textarea>
+                <v-textarea label="Task description" v-model="taskDescription"></v-textarea>
             </v-col>
         </v-row>
     </v-container>
@@ -26,7 +26,7 @@ export default {
     },
     computed: {
         taskName: {
-            // when put in this form, I get 'cannot find property .name' => why?
+            // when put in this form, I get 'cannot find property "name"' => why?
             // () => () {}
             set: function (value) {
                     this.name = value
@@ -34,6 +34,14 @@ export default {
             get: function () {
                     return this.name
                 }
+        },
+        taskDescription: {
+            set: function (value) {
+                    this.description = value
+            },
+            get: function () {
+                    return this.description
+            }
         }
     }
 }
