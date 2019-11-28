2b3deed72a0431c0946b427694bd4666e0342d94
Yurie <cutecupcakeandcute@gmail.com>
2019-11-29 01:32:58 +0900

    set event listener when click event happened in its child component

--- pages/index.vue
+++ pages/index.vue
@@ -2,7 +2,7 @@
   <v-layout column justify-center align-center>
     <v-flex xs12 sm8 md6 />
     <TaskList />
-    <TaskForm />
+    <TaskForm v-on:childToParent="onChildClick"/>
   </v-layout>
 </template>
 
@@ -10,9 +10,20 @@
 import TaskList from '../components/TaskList'
 import TaskForm from '../components/TaskForm'
 export default {
+  data () {
+    return {
+      fromChild: ''
+    }
+  },
   components: {
     TaskList,
     TaskForm
+  },
+  methods: {
+    onChildClick(value) {
+      this.fromChild = value
+      console.log(this.fromChild)
+    }
   }
 }
 </script>
