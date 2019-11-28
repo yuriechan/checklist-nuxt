fb4acff97aa4f5bf7da86578950bf40aad8f5186
Yurie <cutecupcakeandcute@gmail.com>
2019-11-29 08:53:16 +0900

    remove comment

--- components/TaskList.vue
+++ components/TaskList.vue
@@ -14,14 +14,6 @@ export default {
     // Getting the state from store should be done through functions
     // Methods inside the `computed` will be cached, while they will not inside `methods`
 
-    // data must be in function form, not object => why? 
-    // The following code would not work
-    // data: {
-    //     taskList () {
-    //         return this.$store.state.tasks
-    //     }
-    // }
-
     // checkbox binding will be either false or true
     data: () => {
         return {
